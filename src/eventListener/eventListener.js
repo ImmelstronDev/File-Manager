454d7b8cb1bc18e93d2b203d2c7cdad77f4f0418
ImmelstronDev <wishandra.afk@gmail.com>
2024-02-04 21:08:33 +0300

    feat: create hadler for use comand up from CLI

--- src/eventListener/eventListener.js
+++ src/eventListener/eventListener.js
@@ -1,31 +1,39 @@
 import readline from 'node:readline'
 import { EOL } from 'node:os'
+import { NavigationController } from '../navigation/navigationController.js'
 
 export class EventListener {
     constructor(userName) {
         this.userName = userName
         this.goodbyMessage = `Thank you for using File Manager, ${this.userName}, goodbye!` + EOL
-        this.operationFail = 'Operation failed'
+        this.navigation = new NavigationController()
         this.start()
     }
     start() {
         this.rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-        this.rl.on('line', (input)=> {
-            this.viewCurrentWorkDir()
-            if(input.toString().trim() === '.exit') {
-                process.stdout.write(this.goodbyMessage)
-                process.exit(0)
+        this.rl.on('line', async (input)=> {
+            try {
+                await this.navigation.handle(input)
+                this.viewCurrentWorkDir()
+            } catch (error) {
+                console.log(error)
             }
+            
+            // if(input.toString().trim() === '.exit') {
+            //     process.stdout.write(this.goodbyMessage)
+            //     process.exit(0)
+            // }
+            
             
         })
         process.on('exit', ()=> {
             this.rl.close();
             process.stdout.write(this.goodbyMessage)
         })
-       this.rl.on('SIGINT', ()=> {
+        this.rl.on('SIGINT', ()=> {
         this.rl.close();
        })
-       this.rl.on('close', ()=> {
+        this.rl.on('close', ()=> {
         process.exit(0)
        })
     }
--- src/navigation/navigationController.js
+++ src/navigation/navigationController.js
@@ -1,1 +1,22 @@
-export class NavigationController {}
+import { EOL } from 'node:os'
+export class NavigationController {
+    constructor() {
+        this.operationFail = 'Operation failed'
+    }
+
+    async handle(command) {
+        const argsArray = command.split(' ')
+        const firstArg = argsArray[0]
+        // console.log(currentCommand)
+        switch (firstArg) {
+            case 'up':{
+                process.chdir('../')
+                break;
+            }
+            default:{
+                process.stdout.write(`Unknown operation, please enter another command` + EOL);
+                break;
+            }
+        }
+    }
+}
