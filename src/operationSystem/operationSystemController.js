885941b30a7260b72802f4e945fd4a1610c9f007
ImmelstronDev <wishandra.afk@gmail.com>
2024-02-05 20:37:58 +0300

    feat: added error handlers for operationSystemController

--- src/operationSystem/operationSystemController.js
+++ src/operationSystem/operationSystemController.js
@@ -4,11 +4,15 @@ export class OperationSystemController {
     constructor() {
         this.operationFail = 'Operation failed '
     }
-
+    
     async handle(command) {
         const argsArray = command.split(' ')
         argsArray.shift()
         const arg = argsArray[0].trim().slice(2).toLowerCase()
+
+        if(!argsArray.length || argsArray.length !== 1) {
+            process.stdout.write(this.operationFail + 'incorrect enter count of arguments, enter <--argument> after command <os> via space' + EOL)
+        }
 
         switch (arg) {
             case 'eol':{
@@ -19,7 +23,6 @@ export class OperationSystemController {
                 const result = cpus().reduce((arr, cur) => {
                     return [...arr, {model: cur.model, 'clock Rate': `${cur.speed/1000} Ghz`}]
                 }, [])
-                // process.stdout.write(`Amount of CPUS: ${result.length}, model: ${result.model}, clock rate: ${result.clockRate}` + EOL)
                 console.table({'Amount of CPUS': result.length})
                 console.table(result)
                 break;
@@ -38,6 +41,7 @@ export class OperationSystemController {
             }
 
             default:
+                process.stdout.write(`Unknown operation, please enter correct command` + EOL);
                 break;
         }
     }
